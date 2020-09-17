ca9216b993aaf4f418be8e191a13d0e4ecb983e3
Sep Vrij <svrij22@gmail.com>
2020-09-17 12:31:52 +0200

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <nav-bar/>
+        <main class="main-search">
+            <div class="container banner">
+                <span class="overlay-text">Vind nu uw ideale woning op Huisvesting Voor Senioren</span>
+                <img class="overlay-skyline" src="./assets/silhoutte.png"/>
+                <img class="overlay-right" src="./assets/house.png"/>
+            </div>
+            <div class="container search">
+                <label>
+                    <input class="form-control"/>
+                </label>
+            </div>
+        </main>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import NavBar from "@/components/NavBar";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+        name: 'App',
+        components: {
+            NavBar,
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    .main-search {
+        padding-top: 1.5rem;
+        background: linear-gradient(180deg, #ffe3b8 50%, #ffffff 90%);
+
+        & .container {
+            margin: 0 auto;
+            max-width: 64.5rem;
+            display: flex;
+            justify-content: space-between;
+        }
+
+        & .banner {
+            height: 200px;
+            position: relative;
+            flex-direction: row;
+
+            & .overlay-text{
+                padding: 20px 50px;
+                font-size: 35px;
+                text-align: left;
+                font-weight: lighter;
+                font-family: Proxima Nova,sans-serif;
+                z-index: 1;
+            }
+
+            & .overlay-skyline{
+                filter: brightness(0) invert(1);
+                position: absolute;
+                width: 700px;
+                bottom: 10px;
+                right: 0px;
+            }
+
+            & .overlay-right{
+                width: 200px;
+                z-index: 1;
+            }
+        }
+
+        & .search{
+            background-color: #ffd384;
+            border-radius: 4px;
+            min-height: 250px;
+
+            & label{
+                width: 100%;
+            }
+
+            .form-control {
+                display: block;
+                width: 100%;
+                height: calc(1.5em + 0.75rem + 2px);
+                padding: .375rem .75rem;
+                font-size: 1rem;
+                font-weight: 400;
+                line-height: 1.5;
+                color: #495057;
+                background-color: #fff;
+                background-clip: padding-box;
+                border: 1px solid #ced4da;
+                border-radius: .25rem;
+                transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+            }
+        }
+    }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <header class="app-header">
+            <div class="container">
+                <a class="logo" rel="home" href="/">
+                    <img alt="Vue logo" src="../assets/logo.png">
+                </a>
+            </div>
+        </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .app-header{
+        flex-shrink: 0;
+        position: relative;
+        color: #fff;
+        background-color: #ffd384;
+        box-shadow: 0 0.25rem 0.25rem rgba(0,0,0,.25);
+
+        & .container{
+            position: relative;
+            padding: 0 1rem;
+            margin: 0 auto;
+            max-width: 64.5rem;
+
+            display: flex;
+            justify-content: space-between;
+        }
+
+        & .logo {
+            margin: 8px 0;
+
+            & img{
+                height: 36px;
+                width: 101px;
+            }
+        }
+    }
+</style>
